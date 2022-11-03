872f942aae9d303b13c1a07222b22a625d01cb5b
Dang Huy Hoang <dang_huy.hoang@student.unsw.edu.au>
2022-11-03 16:52:28 +0700

    ex

--- src/bin/bai_tap_1.rs
+++ src/bin/bai_tap_1.rs
@@ -1,34 +1,30 @@
-// Bai Tap 1:
-// Cho 2 mảng, kiểm tra mảng này có phải là mảng con của mảng kia không ? (yêu cầu đúng thứ tự của các phần tử)
-// Ví dụ : let org_arr = [1, 2,3,5,6,8, 10, 11];
-//         let sub_arr = [6,8,10];
-fn check_is_sub_array(org_arr: &mut [i32], sub_arr: &mut [i32]) -> bool {
-    let org_arr_length = org_arr.len();
-    let sub_arr_length = sub_arr.len();
-    let mut i = 0;
-    let mut j = 0;
-    while i < org_arr_length && j < sub_arr_length {
-        if org_arr[i] == sub_arr[j] {
-            i = i + 1;
-            j = j + 1;
-            if j == sub_arr_length {
-                return true;
-            }
+/////////////////////////////////////
+// Bài 1
+// Sửa code để compile thành công liên quan tới vấn đề lifetime
+/////////////////////////////////////
+
+use std::io;
+fn main() {
+    let mut input: Vec<&str>;
+    loop {
+        let mut input_text = String::new();
+        println!("Type instruction in the format Add <name> to <department>:");
+        io::stdin()
+            .read_line(&mut input_text)
+            .expect("failed to read from stdin");
+        let trimmed_text: String = input_text.trim().to_string();
+        input = trim(&trimmed_text);
+
+        if input[0] == "Add" && input[2] == "to" {
+            break;
         } else {
-            i = i - j + 1;
-            j = 0;
+            println!("Invalid format.");
         }
     }
-    return false;
+    println!("{:?}", input);
 }
 
-fn main() {
-    let mut org_arr: [i32; 8] = [1, 2, 3, 5, 6, 8, 10, 11];
-    let mut sub_arr: [i32; 3] = [6, 8, 10];
-    let result: bool = check_is_sub_array(&mut org_arr, &mut sub_arr);
-    if result {
-        println!("True! It is a subarray!");
-    } else {
-        println!("False! It's not a subarray");
-    }
+fn trim<'a>(x: &'a str) -> Vec<&'a str> {
+    let vec: Vec<&'a str> = x.split("").collect();
+    vec
 }
--- src/bin/bai_tap_2.rs
+++ src/bin/bai_tap_2.rs
@@ -1,27 +1,40 @@
-//Bài tập 2 : Cho 1 chuỗi ký tự, nhập 1 ký tự từ bàn phím trả về số lần xuất hiện của từ đó trong chuỗi đã cho, và chuỗi không chứa ký tự nhập từ bàn phím. Lưu ý: khong phân biệt viết hoa, viết thường
-//Ví dụ: let input = “adbcdaDd”.
-//Nhập s = ‘a’ => in ra kết quả : 2, “dbcdDd”
-//Nhập s = ‘d’ => in ra kết quả : 4, “abca”
+/////////////////////////////////////
+// Bài 2
+// Implement trait
+// Thực hiên cargo test
+/////////////////////////////////////
 
-use std::io::stdin;
+trait AppendBar {
+    fn append_bar(self) -> Self;
+}
 
-fn bai_tap_2_result(input_string: &mut String) {
-    let mut input_char: String = String::new();
-    stdin().read_line(&mut input_char).unwrap();
-    input_char.truncate(input_char.len() - 1);
-    let mut count: i32 = 0;
-    for elem in input_string.chars() {
-        if elem.to_string().eq(&input_char) {
-            count = count + 1;
-            elem.to_lowercase();
-        }
+impl AppendBar for String {
+    //Add your code here
+    fn append_bar(self) -> Self {
+        self + "Bar"
     }
-
-    let s2 = str::replace(&input_string, &input_char, "");
-    println!("{}, {:?}", count, s2);
 }
 
 fn main() {
-    let mut input_string: String = String::from("adbcdaDd");
-    bai_tap_2_result(&mut input_string);
+    let s = String::from("Foo");
+    let s = s.append_bar();
+    println!("s: {}", s);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_foo_bar() {
+        assert_eq!(String::from("Foo").append_bar(), String::from("FooBar"));
+    }
+
+    #[test]
+    fn is_bar_bar() {
+        assert_eq!(
+            String::from("").append_bar().append_bar(),
+            String::from("BarBar")
+        );
+    }
 }
